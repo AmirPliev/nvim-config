d5b65784ecd83b98199f1f64837ad964ff4a0d89
amir <amir.pliev@avonic.com>
2024-03-07 22:37:01 +0100

    test

--- install.sh
+++ install.sh
@@ -11,30 +11,40 @@ If you don't what this to happen, quit this process now!"
 -----------------------------------------------------------------
 EOF
 
-read -p "Press ENTER to continue..."  result
+# read -p "Press ENTER to continue..."  result
 
-echo "---- Deleting Current NeoVim configurations -----"
-rm -rf ~/.local/share/nvim/
-rm -rf ~/.config/nvim/
+if ! command -v nvimi &> /dev/null
+then
+    echo "NeoVim is not installed. I'll install it first:"
+    add-apt-repository ppa:neovim-ppa/stable    
+    apt-get update
+    apt-get install neovim
+fi 
 
-echo "------ INSTALL VnChad ----"
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 
-
-cp -r $PWD/* ~/.config/nvim/lua/
-cp -r .git/ ~/.config/nvim/lua/
-cp -r .gitignore ~/.config/nvim/lua/
-
-echo ""
-echo "IMPORTANT: "
-echo "Installation Should have been completed. I'm about to delete this folder."
-echo "From now on you can go to ~/.config/nvim/lua/ and perform ./update.sh in order
-      to do updates."
-read -p "Press ENTER to continue..."  result
-
-CURRENT_DIR=$PWD
-cd ~/.config/nvim/lua
+#
+#
+# echo "---- Deleting Current NeoVim configurations -----"
+# rm -rf ~/.local/share/nvim/
+# rm -rf ~/.config/nvim/
+#
+# echo "------ INSTALL VnChad ----"
+# git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 
+#
+# cp -r $PWD/* ~/.config/nvim/lua/
+# cp -r .git/ ~/.config/nvim/lua/
+# cp -r .gitignore ~/.config/nvim/lua/
+#
+# echo ""
+# echo "IMPORTANT: "
+# echo "Installation Should have been completed. I'm about to delete this folder."
+# echo "From now on you can go to ~/.config/nvim/lua/ and perform ./update.sh in order
+#       to do updates."
+# read -p "Press ENTER to continue..."  result
+#
+# CURRENT_DIR=$PWD
+# cd ~/.config/nvim/lua
 # rm -rf $CURRENT_DIR
 
 
 
-nvim
+# nvim
