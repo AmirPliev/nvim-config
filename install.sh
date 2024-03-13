06d89ba93037972bb9d1e75350b985fd4eabcd4f
amir <amir.pliev@avonic.com>
2024-03-13 09:40:17 +0100

    fixed-problems-with-vnchad

--- install.sh
+++ install.sh
@@ -12,37 +12,33 @@ EOF
 
 read -p "Press ENTER to continue..."  result
 
+echo ""
+echo ""
+echo ""
+echo "---- Checking prerequisites -----"
+echo ""
 
-echo "---- Installing prerequisites -----"
+command -v nvim >/dev/null
 
-# Check if NeoVim is installed, if not, install it
-if ! command -v nvim &> /dev/null
-then
-    echo "NeoVim is not installed. I'll install it first:"
-    sudo apt-get install curl
+if [[ $? -ne 0 ]]; then
+    echo "Nvim is not installed"
+    exit 1
+else
+    nvim_version=$(nvim --version | head -1 | grep -o '[0-9]\.[0-9]')
 
-    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-    sudo chmod u+x nvim.appimage
-    sudo mv nvim.appimage /usr/local/bin/nvim
-    sudo chmod +x /usr/local/bin/nvim
-fi 
+    if (( $(echo "$nvim_version < 0.9 " |bc -l) )); then
+            echo "Wrong version of Nvim is installed"
+            exit 1
+    else
+        echo "Nvim version 0.9 or greater is installed"
+    fi
+fi
+
 
 # Check if correct fonts are installed
 if ! command -v fc-list | grep "NerdFont" &> /dev/null
 then
-    echo "There is no NerdFont installed. I'll install it first:"   
-    sudo apt-get install curl
-    curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
-
-    sudo mkdir -p /usr/local/share/fonts/JetBrainsNerdFont
-    sudo apt-get install unzip
-
-    unzip JetBrainsMono.zip -d /usr/local/share/fonts/JetBrainsNerdFont
-
-    sudo apt-get install fontconfig
-    sudo fc-cache -fv
-
-    rm JetBrainsMono.zip
+    echo "There is no NerdFont installed. Make sure you install it or else everything will look weird"   
 fi
 
 sudo apt-get install ripgrep sshfs -y
@@ -57,15 +53,21 @@ if which node > /dev/null
       nvm install node
 fi
 
-
+echo ""
+echo ""
+echo ""
 echo "---- Deleting Current NeoVim configurations -----"
+echo ""
 sudo rm -rf ~/.local/share/nvim/
 sudo rm -rf ~/.config/nvim/
 
+echo ""
+echo ""
+echo ""
 echo "------ INSTALL VnChad ----"
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 
+echo ""
 
-cp -r $PWD/* ~/.config/nvim/lua/
+cp -r nvim ~/.config/nvim
 cp -r .git/ ~/.config/nvim/lua/
 cp -r .gitignore ~/.config/nvim/lua/
 
