491bfe9a909a825fedfa99bcb2f7ee262236d319
AmirPliev <amirpliev@gmail.com>
2024-04-21 12:19:38 +0200

    added-scrolloff-option

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,7 @@ require "nvchad.options"
 vim.opt.relativenumber = true
 
 vim.o.cursorlineopt = "both" -- to enable cursorline!
+vim.opt.scrolloff = 15
 
 vim.g.copilot_no_tab_map = true
 -- Make sure nvim-tree is open on editor open
